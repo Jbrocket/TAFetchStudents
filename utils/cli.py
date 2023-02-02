ddc4c8a085313bfa7ef6e8a8442a14a5c40ea8f5
Jonathan Brockett <jbrocket@nd.edu>
2023-02-02 15:40:15 -0500

    adding cli.py for main driver of cli

--- utils/cli.py
+++ utils/cli.py
@@ -2,10 +2,12 @@ import os
 from directoryManip import CreateDir, RemoveDir
 
 def GetStudentDirs(cli_args: list(str), metadata):
-    if len(cli_args) == 2:
-        if not metadata['currentNDIds']:
-            print("Error: Edit current saved data")
-            os.exit(1)
-        
+    if not metadata['currentNDIds']:
+        return
+    if not metadata["assignmentNum"]:
+        return
+    if not metadata['']:
+        return
+    
     for NDid in metadata['currentNDIds']:
         CreateDir(NDid, metadata['assignmentNum'])
